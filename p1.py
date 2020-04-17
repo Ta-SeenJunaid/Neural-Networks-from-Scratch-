0283de16aae165c2a0330d34716036f89b7b86c8
Ta-SeenJunaid <taseen.junaid@gmail.com>
2020-04-18 05:04:44 +0600

    4 Input

--- p1.py
+++ p1.py
@@ -1,9 +1,10 @@
-inputs = [1.5, 4.5, 2.4]
+inputs = [1.5, 4.5, 2.4, 1.3]
 
-weights = [4.5, 7.9, 2.1]
+weights = [4.5, -1.9, 2.1, 1.9]
 
 bias = 3
 
-output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
+output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] \
+         + inputs[3]*weights[3] + bias
 
 print(output)
