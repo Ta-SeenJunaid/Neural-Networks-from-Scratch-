b7375cdb7262bd64f77492a03b4b7b7983ecf341
Ta-SeenJunaid <taseen.junaid@gmail.com>
2020-04-25 11:04:47 +0600

    Looping for output

--- p3.py
+++ p3.py
@@ -4,5 +4,16 @@ weights = [[2, 0.2, 5, 2],
            [2.1, 2, 1.5, -2],
            [2, 1.3, 5, 3.1]]
 
-biases = [0.2, 3, 0.5]
+biases = [0.2, 3, -2]
 
+output = []
+
+for nn_weights, nn_biases in zip(weights, biases):
+    product = 0
+
+    for nn_inputs, nn_w in zip(inputs, nn_weights):
+        product += nn_inputs*nn_w
+
+    output.append(product + nn_biases)
+
+print(output)
