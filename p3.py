0c20b6be0864a9b9c198190b0dd5128f6603d9b7
Ta-SeenJunaid <taseen.junaid@gmail.com>
2020-04-25 12:22:56 +0600

    Introducing numpy

--- p3.py
+++ p3.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 inputs = [1.1, 1.3, -2, 2]
 
 weights = [[2, 0.2, 5, 2],
@@ -6,14 +8,7 @@ weights = [[2, 0.2, 5, 2],
 
 biases = [0.2, 3, -2]
 
-output = []
-
-for nn_weights, nn_biases in zip(weights, biases):
-    product = 0
-
-    for nn_inputs, nn_w in zip(inputs, nn_weights):
-        product += nn_inputs*nn_w
-
-    output.append(product + nn_biases)
+output = np.dot(weights, inputs) + biases
 
 print(output)
+
