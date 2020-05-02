aeb7cbf7d61ddf670ad8a19a1099f2a3c9f95424
Ta-SeenJunaid <taseen.junaid@gmail.com>
2020-05-02 13:14:36 +0600

    Adding 2nd layer

--- p4.py
+++ p4.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 inputs = [[1, 2.3, 4, 2.5],
-     [2, 3.7, 0.2, 4.3],
-     [1,3.2, 2, 2.6]]
+          [2, 3.7, 0.2, 4.3],
+          [1, 3.2, 2, 2.6]]
 
 weights = [[2, 0.2, 5, 2],
            [2.1, 2, 1.5, -2],
@@ -10,6 +10,14 @@ weights = [[2, 0.2, 5, 2],
 
 biases = [0.2, 3, -2]
 
-output = np.dot(inputs,np.array(weights).T) + biases
+weights2 = [[2, 0.2, 5],
+           [2.1, 2, 1.5],
+           [2, 1.3, 5]]
 
-print(output)
+biases2 = [0.2, 3, -2]
+
+layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
+
+layer2_outputs = np.dot(layer1_outputs, weights2) + biases2
+
+print(layer2_outputs)
