2ac0f88a523460c0ced495e3244bc2153fcd9577
Ta-SeenJunaid <taseen.junaid@gmail.com>
2020-05-02 12:57:14 +0600

    Batch of input

--- p4.py
+++ p4.py
@@ -1,5 +1,15 @@
 import numpy as np
 
-x = [[1, 2.3, 4, 2.5],
+inputs = [[1, 2.3, 4, 2.5],
      [2, 3.7, 0.2, 4.3],
      [1,3.2, 2, 2.6]]
+
+weights = [[2, 0.2, 5, 2],
+           [2.1, 2, 1.5, -2],
+           [2, 1.3, 5, 3.1]]
+
+biases = [0.2, 3, -2]
+
+output = np.dot(inputs,np.array(weights).T) + biases
+
+print(output)
